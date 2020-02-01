d8a8be25394a0bf6370d6d4cdbd0ce48cebc5ff6
lorienb <lorien.boyce@hotmail.com>
2020-02-01 11:40:19 -0500

    Fancier menu scene - still need to fix mousehover over textMesh

---
+++ Assets/Scripts/CursorHover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorHover : MonoBehaviour
+{
+    TextMesh textMesh;
+    // Start is called before the first frame update
+    void Start(){
+	    textMesh = GetComponent<TextMesh>();
+        textMesh.color = Color.black;
+    }
+
+    void OnMouseEnter(){
+	    textMesh.color = Color.red;
+    }
+
+    void OnMouseExit() {
+	    textMesh.color = Color.black;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public bool isStart;
+    public bool isHowToPlay;
+
+    // Change from OnMouseUp to when button is pressed or something
+    void OnMouseUp(){ 
+        if(isStart)
+	    {
+		    SceneManager.LoadScene(1); // Change number later
+	    }
+	    if (isHowToPlay)
+	    {
+	    	SceneManager.LoadScene(2); // Change number later
+	    }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
