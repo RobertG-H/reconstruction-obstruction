2efa607af642595c87a45b99408bbe205a74eff0
reidaaron <reidaaront@gmail.com>
2020-02-01 00:02:25 -0500

    Create camera base script

---
+++ Assets/CameraBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBase : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject fort;
+    private Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = CameraBase;
+        size = cam.orthographicSize;
+    }
+
+    void ZoomOut()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
