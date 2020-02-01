4dc74a95e53a5b34cf3ff572e5490de3d2bb4d78
Joanne Jung <JJDaeun@users.noreply.github.com>
2020-02-01 16:13:53 -0500

    Homing Projectile

---
+++ Assets/Scripts/EnemyScripts/EnemyCannon.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCannon : MonoBehaviour
+{
+    private bool fix; // true if cannon is fixed 
+
+    public Rigidbody2D cannonball;
+    public Transform cannonHeadTransform;
+    public Transform playerTransform; 
+
+    private float hSpeed;
+    private float vSpeed; 
+
+    private int timePassed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // :) 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // add while (fixed) 
+        if (timePassed % 50 == 0)
+        {
+            vSpeed = (playerTransform.position.y - cannonHeadTransform.position.y);
+            hSpeed = (playerTransform.position.x - cannonHeadTransform.position.x);
+
+            Rigidbody2D cannonballInstance; 
+            cannonballInstance = Instantiate(cannonball, cannonHeadTransform.position, cannonHeadTransform.rotation); 
+            cannonballInstance.velocity = new Vector3(hSpeed, vSpeed, 0);
+        }
+        timePassed++;
+    }
+}
---
+++ Assets/Scripts/EnemyScripts/EnemyCannonballDestroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCannonballDestroy : MonoBehaviour
+{
+    public float timeoutDestructor = 1.5f;
+    public GameObject cannonballInstance; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(cannonballInstance, 1.0f); 
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(cannonballInstance); 
+    }
+}
