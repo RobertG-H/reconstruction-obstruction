54e4f251611baa611cba95d350ba70431ec3daa6
lorienb <lorien.boyce@hotmail.com>
2020-02-02 14:11:37 -0500

    Main menu scene complete

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,17 +6,12 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public bool isStart;
-    public bool isHowToPlay;
 
     // Change from OnMouseUp to when button is pressed or something
     void OnMouseUp(){ 
         if(isStart)
 	    {
 		    SceneManager.LoadScene(1); // Change number later
-	    }
-	    if (isHowToPlay)
-	    {
-	    	SceneManager.LoadScene(2); // Change number later
 	    }
     }
 
