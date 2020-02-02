97975b9846d33715eff05d1186a29319ee0d8221
reidaaron <reidaaront@gmail.com>
2020-02-02 12:25:47 -0500

    Update piledriver projectile

--- Assets/Scripts/PileDriver.cs
+++ Assets/Scripts/PileDriver.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class PileDriver : AttackController
 {
-    public GameObject piledriverProjectile; //Pipe driver game object
+    public GameObject[] piledriverProjectiles; //Pipe driver game object
     [SerializeField]
     private float projectileForce = 10f;
     [SerializeField]
-    private float projectileStartingPosition = 1f;
+    private float projDisplacement = 10f;
+    [SerializeField]
+    private float projFloor = 68f;
+    [SerializeField]
+    private float deleteAfter = 1.5f;
+    private float delay;
+    private float[] delaytimes = new float[] {3f,3.5f,4f,4.5f,5f,5.5f,6f};
+    private bool delayed = false;
 
     void Start()
     {
@@ -17,8 +24,39 @@ public class PileDriver : AttackController
 
     void ShootShockwave()
     {
-        GameObject shockwave1 = Instantiate(piledriverProjectile, new Vector3(projectileStartingPosition, 0, 0), Quaternion.identity) as GameObject;
-        shockwave1.GetComponent<Rigidbody2D>().velocity = new Vector3(projectileForce, 0, 0);
+        GameObject shockwaveLeft = Instantiate(piledriverProjectiles[0], new Vector2(transform.position.x - projDisplacement, -1*projFloor), Quaternion.identity) as GameObject;
+        shockwaveLeft.GetComponent<Rigidbody2D>().velocity = new Vector3(projectileForce * -1, 0, 0);
+        GameObject shockwaveRight = Instantiate(piledriverProjectiles[1], new Vector2(transform.position.x + projDisplacement, -1*projFloor), Quaternion.identity) as GameObject;
+        shockwaveRight.GetComponent<Rigidbody2D>().velocity = new Vector3(projectileForce, 0, 0);
+        Destroy(shockwaveLeft, deleteAfter);
+        Destroy(shockwaveRight, deleteAfter);
+    }
+
+    void Update()
+    {
+        // if ('nail is hit')
+        // {
+            if (!delayed)
+            {
+                StartCoroutine(DelayAttack());
+            }
+        // }
+        
+    }
+
+    void Attack()
+    {
+        //play attack animation
+        ShootShockwave();
+    }
+
+    IEnumerator DelayAttack()
+    {
+        delayed = true;
+        delay = delaytimes[Random.Range(0, delaytimes.Length)];
+        yield return new WaitForSeconds(delay);
+        Attack();
+        delayed = false;
     }
 
 }
