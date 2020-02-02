ee02e27389f5bc048cffc0a6732cb456b7db9973
RobertG-H <robertgrahamhu@gmail.com>
2020-02-02 16:59:57 -0500

    start of yeti intro

---
+++ Assets/intro_script.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class intro_script : MonoBehaviour
+{
+
+    public SpriteRenderer intro;
+    public AudioSource introSound;
+    private IEnumerator coroutine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coroutine = Fade(2.0f);
+        StartCoroutine(coroutine);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // every 2 seconds perform the print()
+    private IEnumerator Fade(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime / 2);
+            print("WaitAndPrint " + Time.time);
+            yield return new WaitForSeconds(waitTime / 2);
+
+        }
+    }
+}
